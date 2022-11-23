e27d1b5ff155c7ac963ea4dca412c1793c54e172
zbialaz <eduardobialas74@gmail.com>
2022-11-22 21:12:20 -0300

    Criada Render e Método q renderiza frontend pages

--- src/View/Render.php
+++ src/View/Render.php
@@ -2,7 +2,39 @@
 
  namespace Petshop\View;
 
+use Exception;
+
  class Render
  {
+    /**
+     * Método que carrega uma página com a estrutura FrontEnd,
+     * recebe dois parâmetros:
+     *
+     * @param string $pagina Nome do arquivo a ser impresso
+     * @param array $dados Dados a serem inseridos na página
+     * @return void
+     */
+    static public function front(string $pagina, array $dados = [])
+    {
+        //monta o caminho local onde a página solicitada está
+        $pathPagina = TFRONTEND . 'pages/' . $pagina . '.php';
 
+        if ( !file_exists($pathPagina) ) {
+            error_log('Página template não localizada em: '.$pathPagina);
+            throw new Exception("A página solicitada '{$pagina}' não foi localizada");
+        }
+
+        if ( empty($dados['titulo']) ) {
+        $dados['titulo'] = FRONTEND_TITLE;
+        } else {
+            $dados['titulo'] = $dados['titulo'] . ' - ' . FRONTEND_TITLE;
+        }
+
+        //transforma os índices de vetores em variáveis 
+        extract($dados);
+        
+        require_once TFRONTEND . 'common/top.php';
+        require_once $pathPagina;
+        require_once TFRONTEND . 'common/botton.php';
+    }
  }
