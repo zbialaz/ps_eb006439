ae600d3f4550cf9c4cb0909cfa373727a5f9bea1
zbialaz <eduardobialas74@gmail.com>
2022-11-24 19:45:08 -0300

    Criada a classe de Estado

---
+++ src/Model/Estado.php
@@ -0,0 +1,46 @@
+<?php 
+
+namespace Petshop\Model;
+
+use Petshop\Core\Attribute\Entidade;
+use Petshop\Core\Attribute\Campo;
+use Petshop\Core\DAO;
+
+//dicas
+#[Entidade(name: 'estados')]
+
+class Estado extends DAO
+{
+
+    #[Campo(label:'UF', nn:true, pk:true)]
+    protected $uf;
+
+    #[Campo(label:'IBGE', nn:true)]
+    protected $ibge;
+
+    #[Campo(label:'Estado', nn:true, order:true )]
+    protected $estado;
+
+    #[Campo(label:'Região', nn:true)]
+    protected $regiao;
+
+    public function getUf()
+    {
+        return $this->uf;
+    }
+
+    public function getIbge()
+    {
+        return $this->ibge;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function getRegiao()
+    {
+        return $this->regiao;
+    } 
+}
