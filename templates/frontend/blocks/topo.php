a380f70b2b7601078c1df64e5f0798dc780d532f
zbialaz <eduardobialas74@gmail.com>
2022-12-12 21:59:18 -0300

    Template Backend

--- templates/frontend/blocks/topo.php
+++ templates/frontend/blocks/topo.php
@@ -32,42 +32,32 @@
          </div>
        </div>
        <div class="topo-site-inferior row ">
-            <div class="topo-site-inferior-menu col-2">
-                <a data-bs-toggle="offcanvas" href="#offcanvas-menu" aria-controls="offcanvas-menu" class="d-flex align-items-center">
-                    <i class="bi bi-list fs-3 pe-1"></i>
-                    <span>Departamentos</span>
-                </a>
-            </div>
-            <div class="topo-site-inferior-contatos col-6 row">
-                <div class="col-auto d-flex align-items-center">
-                    <a href="/nossas-lojas" title="Conheça as nossas lojas">
-                        <i class="bi bi-geo-alt-fill pe-1"></i>
-                        <span>Nossas Lojas</span>
-                    </a>
-                </div>
-                <div class="col-auto d-flex align-items-center">
-                    <a href="/fale-conosco" title="Fale Conosco">
-                        <i class="bi bi-megaphone-fill pe-1"></i>
-                        <span>Fale Conosco</span>
-                    </a>
-                </div>
-            </div>
-            <div class="topo-site-inferior-fone col-4 d-flex align-items-center justify-content-end">
-                <i class="bi bi-telephone-fill pe-2"></i>
-                <span>(55) 99143-3893</span>
-            </div>
-        </div>
-    </div>
-</div>
+         <div class="topo-site-inferior-menu col-2">
+           <a class="d-flex align-items-center" data-bs-toggle="offcanvas" href="#offcanvas-menu" aria-controls="offcanvas-menu">
+             <i class="bi bi-list fs-3 pe-1"></i>
+             <span>Departamentos</span>
+           </a>
+         </div>
+         <div class="topo-site-inferior-contatos col-6 row">
+           <div class="col-auto d-flex align-items-center">
+             <a href="/nossas-lojas" title="Conheça as nossas lojas">
+               <i class="bi bi-geo-alt-fill pe-1"></i>
+               <span>Nossas Lojas</span>
+             </a>
+           </div>
+           <div class="col-auto d-flex align-items-center">
+             <a href="/fale-conosco" title="Fale Conosco">
+               <i class="bi bi-megaphone-fill pe-1"></i>
+               <span>Fale Conosco</span>
+             </a>
+           </div>
+         </div>
+         <div class="topo-site-inferior-fone col-4 d-flex align-items-center justify-content-end">
+           <i class="bi bi-telephone-fill pe-2"></i>
+           <span>(55) 99143-3893</span>
+         </div>
+       </div>
+     </div>
+   </div>
 
-<div class="offcanvas offcanvas-start rounded-2 m-2" data-bs-scroll="true" tabindex="-1" id="offcanvas-menu" aria-labelledby="offcanvas-menuLabel">
-    <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvas-menuLabel">Categorias do site</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body">
-        <p>Lista das categorias 1</p>
-        <p>Lista das categorias 2</p>
-        <p>Lista das categorias 3</p>
-    </div>
-</div>
+   
