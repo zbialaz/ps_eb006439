79c4b7819bb44b0e6544660731a2e94d37076284
zbialaz <eduardobialas74@gmail.com>
2023-02-17 13:14:07 -0300

    Login

--- templates/frontend/pages/admin-login.php
+++ templates/frontend/pages/admin-login.php
@@ -2,7 +2,6 @@
     <div class="row">
         <div class="col-6 mx-auto">
             <h1 class="text-center display-6 pb-5">Faça seu login - Administrativo</h1>
-            <?=retornaHTMLAlertMensagemSessao()?>
             <?= $formLogin ?>
         </div>
     </div>
